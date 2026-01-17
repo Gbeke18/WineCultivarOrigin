12950a3b6ee9d4073bc699444c0ef7ff249ea29b
Gbeke18 <gbekeiyun@gmail.com>
2026-01-17 02:43:08 +0100

    Update app.py

--- Wine Cultivar origin/app.py
+++ Wine Cultivar origin/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "model", "wine_cultivar_model.pkl")
+
 # Load model
-with open("wine_cultivar_model.pkl", "rb") as f:
-    model, scaler, features = pickle.load(f)
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -23,3 +27,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
